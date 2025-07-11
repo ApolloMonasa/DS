aa6cfec7157369756d0482c20e1aa28ab6c6a9f8
ApolloMonasa <2784761245@qq.com>
2025-07-12 07:34:00 +0800

    feat : P1433

---
+++ Algorithm_AND_DataStructure/C11_BacktrackingPblm/7.P1433.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: 7.P1433.cpp
+	> Author: HDU-Apollo
+	> Mail: xyl6716@outlook.com
+	> Created Time: Sat 12 Jul 2025 07:33:40 AM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<vector>
+#include<string>
+#include<stack>
+#include<set>
+#include<map>
+#include<cmath>
+#include<cstring>
+#include<climits>
+#include<algorithm>
+#include<iomanip>
+#include<queue>
+#include<numeric>
+using namespace std;
+
+int main() {
+    return 0;
+}
