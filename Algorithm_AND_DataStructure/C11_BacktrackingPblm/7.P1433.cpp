ad49de1a9465919213684ad03dbdfc96652dc0ba
ApolloMonasa <2784761245@qq.com>
2025-07-21 20:28:11 +0800

    feat : P1433 吃奶酪

--- Algorithm_AND_DataStructure/C11_BacktrackingPblm/7.P1433.cpp
+++ Algorithm_AND_DataStructure/C11_BacktrackingPblm/7.P1433.cpp
@@ -21,7 +21,51 @@
 #include<queue>
 #include<numeric>
 using namespace std;
+const int N = 21;
+int n;
+double x[N], y[N];
+double dis[N][N];
+#define S(i) ((i) * (i))
+double __dis(int i, int j) {
+    return sqrt(S(x[i] - x[j]) + S(y[i] - y[j]));
+}
+double dp[70000][20];
+double ans = 1e9 + 7;
+int ind[70000];
+#define MUSK(i) ((1 << i + 1) - 2)
+
+void dfs(int t, int now, double s) {
+    if (t == 0) {
+        ans = min(ans, s);
+        return ;
+    }
+    for (int k = t; k; k &= k - 1){
+        int to = ind[k & -k], next_t = t - (1 << to);
+        double l = s + dis[now][to];
+        if (dp[next_t][to] != 0 && dp[next_t][to] <= l) continue;
+        dp[next_t][to] = l;
+        if (ans <= l) continue;
+        dfs(next_t, to, l);
+    }
+    return ;
+}
 
 int main() {
+    scanf("%d", &n);
+    x[0] = y[0] = 0;
+    double a, b;
+    for (int i = 1; i <= n; i++) {
+        scanf("%lf%lf", &a, &b);
+        x[i] = a;
+        y[i] = b;
+    }
+    for (int i = 0; i <= n; i++) {
+        for (int j = i; j <= n; j++) {
+            dis[i][j] = dis[j][i]  = __dis(i, j);
+        }
+    }
+    for (int k = 1, i = 0; i <= 16; i++, k <<= 1) ind[k] = i;
+    dfs(MUSK(n), 0, 0);
+    printf("%.2lf\n", ans);
     return 0;
 }
